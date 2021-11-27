9167508ef3b83c901d46f5f029afa2d8d74b459f
David-Lor <17401854+David-Lor@users.noreply.github.com>
2021-11-27 17:19:00 +0100

    test: build from pyston & pypy

--- tools/tests/test_build.py
+++ tools/tests/test_build.py
@@ -24,7 +24,7 @@ class TestBuild(BaseTest):
         output = self.run_container(image=image, final_args=["pwd"])
         assert "/home/{}".format(USERNAME) in output
 
-    def test_build_tag_alpine(self):
+    def test_build_from_alpine(self):
         """Test building the image using the 'python:alpine' base image
         """
         image, output = self.build_image(from_image="python:alpine")
@@ -33,7 +33,7 @@ class TestBuild(BaseTest):
         output = self.run_container(image=image, args=["--entrypoint", "which"], final_args=["apk"])
         assert "/sbin/apk" in output
 
-    def test_build_python_27(self):
+    def test_build_from_python27(self):
         """Test building the image using the 'python:2.7-alpine' base image
         """
         image, output = self.build_image(from_image="python:2.7-alpine")
@@ -41,3 +41,21 @@ class TestBuild(BaseTest):
 
         output = self.run_container(image=image, final_args=["python", "-V"])
         assert output.startswith("Python 2.7")
+
+    def test_build_from_pyston(self):
+        """Test building the image using the 'pyston/pyston' base image
+        """
+        image, output = self.build_image(from_image="pyston/pyston")
+        assert image in output
+
+        output = self.run_container(image=image, final_args=["python", "-V"])
+        assert "[Pyston " in output
+
+    def test_build_from_pypy(self):
+        """Test building the image using the 'pypy' base image
+        """
+        image, output = self.build_image(from_image="pypy")
+        assert image in output
+
+        output = self.run_container(image=image, final_args=["python", "-V"])
+        assert "[PyPy " in output
